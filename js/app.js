b81f9956f8bc20e7b2c8df0c996e318ca8054cfb
fadiHB <eng.fadi.hb@gmail.com>
2020-10-20 23:35:44 +0300

    part1-not-done

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 'use strict';
 
-
+const all = [];
 function Horns (data) {
   this.image_url = data.image_url,
   this.title = data.title,
   this.description = data.description,
   this.keyword = data.keyword,
   this.horns = data.horns;
+  all.push(this);
 
 }
 
-Horns.prototype.render = function () {
+// randering manually
+Horns.prototype.render1 = function () {
 
   let photoTemplateCloned = $('#photo-template').clone();
   $('main').append(photoTemplateCloned);
   //   photoTemplateCloned.className=this.keyword;
   //   photoTemplateCloned.id = this.keyword;
+
+
 
   photoTemplateCloned.html (
 
@@ -37,27 +41,36 @@ Horns.prototype.render = function () {
 
 };
 
+
+Horns.prototype.render2 = function () {
+  let template = $('#div2').html();
+  let html = Mustache.render(template,this);
+  return html;
+};
+
+
 const ajaxSettings = {
   method: 'get',
   dataType: 'json'
 };
 
-$.ajax ('../data/page-1.json',ajaxSettings).then ( data => {
-  data.forEach(element => {
+// $.ajax ('../data/page-1.json',ajaxSettings).then ( data => {
+//   data.forEach(element => {
 
-    let horn = new Horns (element);
-    horn.render();
+//     let horn = new Horns (element);
+//     horn.render1();
+//     console.log(horn.render2());
 
-    let optionEl = $(`<option value = "${horn.keyword}">${horn.keyword}</option>`);
-    $('select').append(optionEl);
+//     let optionEl = $(`<option value = "${horn.keyword}">${horn.keyword}</option>`);
+//     $('#KeywordSelector').append(optionEl);
 
 
-  });
+//   });
 
-});
+// });
 
 $(document).ready(function(){
-  $('select').on('change', function(event){
+  $('#KeywordSelector').on('change', function(event){
 
     let selected = event.target.value; // selected = value === className
     console.log(selected);
@@ -66,29 +79,99 @@ $(document).ready(function(){
     $(`.${selected}`).fadeIn(1000);
 
   });
+
+  let sortOption1 = $(`<option id="sortedKey">Sort By</option>`);
+  let sortOption2 = $(`<option id="sortedKey">Sort By</option>`);
+  $('#sortSelector').append(sortOption1,sortOption2);
+
+
+});
+
+$(function () {
+
+  $('form').on('click' , function(event) {
+    let pageEl = event.target.id;
+
+
+    if ( pageEl === 'inputPg1' ) {
+
+      $.ajax ('../data/page-1.json',ajaxSettings).then ( data => {
+        data.forEach(element => {
+
+
+
+          let horn = new Horns (element);
+          horn.render2();
+          console.log(horn.render1());
+
+          let optionEl = $(`<option value = "${horn.keyword}">${horn.keyword}</option>`);
+          $('#KeywordSelector').append(optionEl);
+
+
+
+
+        });
+
+      });
+
+    }
+
+
+    else if ( pageEl === 'inputPg2' ) {
+
+      $.ajax ('../data/page-2.json',ajaxSettings).then ( data => {
+        data.forEach(element => {
+
+
+
+          let horn = new Horns (element);
+          horn.render2();
+          console.log(horn.render1());
+
+          let optionEl = $(`<option value = "${horn.keyword}">${horn.keyword}</option>`);
+          $('#KeywordSelector').append(optionEl);
+
+
+
+
+
+        });
+
+      });
+
+
+    }
+
+  } );
+
+
+
+
 });
 
 
 
-// $(document).ready(function(){
 
-//   all.forEach(element => {
-//     let optionEl = `<option>${element.title}</option>`;
-//     console.log(optionEl);
-//     $('select').append('optionEl');
+
+
+
+
+// // Using Mustache
+// Neighborhood.prototype.render = function(){
+//   // first we get the template from the HTML doc
+//   let template = $('#neighborhood-template').html();
+//   // Secondly we use mustaache render function to merge the template with the given data
+//   let html = Mustache.render(template, this);
+//   return html;
+// };
+
+
+// $('hornes').click(function () {
+
+//   all.sort((a,b) => a.horns-b.horns );
+//   all.forEach (element => {
+//     let n = new Horns (element);
+
 //   });
-
-// });
-
-
-
-// for (let i = 0; i < all.length; i++) {
-//   let optionEl = $('<option>' + all[i].title + '</option>');
-//   $('main').append('select');
 // }
-
-// all.forEach(element => {
-//   let optionEl = `<option>${element.title}</option>`;
-//   console.log(optionEl);
-//   $('select').append('optionEl');
-// });
+// );
